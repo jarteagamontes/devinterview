d842c44138f6414167909fdcdf34b9a7da034a45
root <root@bastion.ocpv.baz.kio.local>
2024-12-09 08:07:18 -0600

    v4qa

--- vm-aws/main.tf
+++ vm-aws/main.tf
@@ -54,4 +54,9 @@ output "altair_instance_public_ip" {
   value       = aws_instance.altair_ec2_instance.public_ip
   description = "Public IP of the EC2 instance"
 }
+output "altair_instance_id" {
+  value       = aws_instance.altair_ec2_instance.id
+  description = "ID of the EC2 instance"
+}
+
 
