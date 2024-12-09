5d3a1406ba9165a1c784a37ee40411c3b1b29aee
root <root@bastion.ocpv.baz.kio.local>
2024-12-09 07:00:42 -0600

    v9

--- vm-aws/outputs.tf
+++ vm-aws/outputs.tf
@@ -3,9 +3,3 @@ output "altair_instance_public_ip" {
   description = "Public IP of the EC2 instance"
 }
 
-output "altair_private_key" {
-  value       = tls_private_key.altair_ec2_key_pair.private_key_pem
-  description = "Private key for the EC2 instance"
-  sensitive   = true
-}
-
